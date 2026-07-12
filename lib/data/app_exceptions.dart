e0251f350c29834908f354318289b58dc1a252cb
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-07-12 09:06:06 +0500

    Network APIs Services

--- lib/data/app_exceptions.dart
+++ lib/data/app_exceptions.dart
@@ -2,7 +2,7 @@ class AppExceptions implements Exception {
   final String? _message;
   final String? _prefix;
 
-  AppExceptions({this._message, this._prefix});
+  AppExceptions([this._message, this._prefix]);
 
   @override
   String toString() {
@@ -12,18 +12,18 @@ class AppExceptions implements Exception {
 
 class FetchDataException extends AppExceptions {
   FetchDataException([String? message])
-    : super(message: 'Error During Communication');
+    : super(message, 'Error During Communication');
 }
 
 class BadRequestException extends AppExceptions {
-  BadRequestException([String? message]) : super(message: 'Invalid request');
+  BadRequestException([String? message]) : super(message, 'Invalid request');
 }
 
 class UnAuthorizedException extends AppExceptions {
   UnAuthorizedException([String? message])
-    : super(message: 'UnAuthorized request');
+    : super(message, 'UnAuthorized request');
 }
 
 class InvalidInputException extends AppExceptions {
-  InvalidInputException([String? message]) : super(message: 'Invalid Input');
+  InvalidInputException([String? message]) : super(message, 'Invalid Input');
 }
