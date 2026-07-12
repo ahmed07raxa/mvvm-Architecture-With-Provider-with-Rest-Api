4edc3531a5c985d9f4a97c62bc7daf180695dfac
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-07-12 10:16:51 +0500

    Named Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_architecture_with_provider_with_restapi/utils/routes/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,17 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MVVM - Architecture',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-
+      theme: ThemeData(useMaterial3: true, primaryColor: Colors.blue),
+      initialRoute: AppRoutes.loginScreen,
+      routes: AppRoutes.getRoutes(),
     );
   }
 }
---
+++ lib/utils/routes/app_routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_architecture_with_provider_with_restapi/view/home_screen.dart';
+import 'package:mvvm_architecture_with_provider_with_restapi/view/login_screen.dart';
+
+class AppRoutes {
+  static const loginScreen = "/loginScreen";
+  static const homeScreen = "/homeScreen";
+
+  static Map<String, Widget Function(BuildContext)> getRoutes() => {
+    loginScreen: (context) => LoginScreen(),
+    homeScreen: (context) => HomeScreen(),
+  };
+}
