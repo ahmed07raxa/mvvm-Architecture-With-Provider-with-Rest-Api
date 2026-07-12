0e41ca5b9ed55756a02bcf1c01f63c27a715f40f
ahmed07raxa <ahmedgoofy6@gmail.com>
2026-07-12 10:48:32 +0500

    Utils

---
+++ lib/utils/utils.dart
@@ -0,0 +1,35 @@
+import 'package:another_flushbar/flushbar_route.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:another_flushbar/another_flushbar.dart';
+
+class Utils {
+  static void toastMessage(String message) {
+    Fluttertoast.showToast(msg: message);
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        forwardAnimationCurve: Curves.decelerate,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(15),
+        message: message,
+        duration: Duration(seconds: 3),
+        borderRadius: BorderRadius.circular(20),
+        flushbarPosition: FlushbarPosition.TOP,
+        backgroundColor: Colors.red,
+        reverseAnimationCurve: Curves.easeInOut,
+        positionOffset: 20,
+        icon: Icon(Icons.error, size: 28, color: Colors.white),
+      )..show(context),
+    );
+  }
+
+  static ScaffoldMessenger snackBar(String message, BuildContext context) {
+    return ScaffoldMessenger(
+      child: SnackBar(backgroundColor: Colors.red, content: Text(message)),
+    );
+  }
+}
--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_architecture_with_provider_with_restapi/utils/routes/app_routes.dart';
+import 'package:mvvm_architecture_with_provider_with_restapi/utils/utils.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Utils.flushBarErrorMessage('messageasdsda', context);
+            },
+            child: Text('CLICK'),
+          ),
+        ],
+      ),
+    );
   }
 }
